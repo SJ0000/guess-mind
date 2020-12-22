7cfb12b06da21795c4c68ed353f9666457a76925
SUNGJIN.KANG <slaayer60@gmail.com>
2020-12-22 23:57:23 +0900

    ~ 3.3

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,62 @@
+/* eslint-disable prettier/prettier */
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
+import babel from "babelify";
+
+sass.compiler = require("node-sass");
 
 const paths = {
   styles: {
     src: "assets/scss/styles.scss",
-    dest: "src/static/styles"
-  }
+    dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss",
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js",
+  },
 };
 
-export function styles() {
-  return gulp
+const clean = () => del(["src/static"]);
+
+const styles = () =>
+  gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false,
+      }),
+    )
+    .pipe(minifyCSS()) // 만들어지는 css파일 띄어쓰가같은거 없애서 용량 줄여줌
     .pipe(gulp.dest(paths.styles.dest));
-}
+
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babel.configure({
+            presets: ["@babel/preset-env"],
+          }),
+        ],
+      }),
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+const watchFiles = () => {
+  gulp.watch(paths.js.watch, js);
+  gulp.watch(paths.styles.watch, styles);
+};
+const dev = gulp.series([clean, styles, js, watchFiles]);
+
+export const build = gulp.series([clean, styles, js]);
+
+export default dev;
